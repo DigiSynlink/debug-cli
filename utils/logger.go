fb6b89ea72ff8600e60498dd449e629da2752498
NeverBehave <gayhub@never.pet>
2022-08-04 03:18:21 -0700

    first commit

---
+++ utils/logger.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"sync"
+
+	"github.com/sirupsen/logrus"
+)
+
+var logger *logrus.Logger
+var once sync.Once
+
+// start loggeando
+func GetInstance() *logrus.Logger {
+	once.Do(func() {
+		logger = createLogger()
+	})
+	return logger
+}
+
+func createLogger() *logrus.Logger {
+	return logrus.New()
+}
